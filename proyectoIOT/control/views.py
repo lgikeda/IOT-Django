12492226c3c0e5b3936b802cf85ad3d3ac1c1b75
lgikeda <126200849+lgikeda@users.noreply.github.com>
2024-01-14 14:45:34 +0100

    Add files via upload

---
+++ proyectoIOT/control/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Control
+from .forms import ControlForm
+
+# Create your views here.
+def crear_control(request):
+    if request.method == 'POST':
+        form = ControlForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('controls')
+    else:
+        form = ControlForm()
+
+    return render(request, 'control/crear_control.html', {
+        'title': 'Crear Control',
+        'form': form
+    })
+
+def mostrar_controles(request):
+    controles = Control.objects.all()
+    return render(request, 'control/controls.html', {
+        'title': 'Controles',
+        'controles': controles
+        })
+
+def cambiar_estado(request, control_id):
+    control = get_object_or_404(Control, id=control_id)
+    control.estado = not control.estado  # Cambia el estado al contrario (True a False, False a True)
+    control.save()
+    return redirect('controls')  # Redirige de nuevo a la página de controles
+
+def borrarControl(request, id):
+    control = Control.objects.get(pk=id)
+    control.delete()
+
+    return redirect('controls')
