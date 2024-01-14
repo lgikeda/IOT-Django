c43f1db743e146549972fd112f9726cf17a4a346
lgikeda <126200849+lgikeda@users.noreply.github.com>
2024-01-14 19:15:31 +0100

    Update urls.py

--- control/urls.py
+++ control/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('controls/', views.mostrar_controles, name='controls'),
     path('cambiar_estado/<int:control_id>/', views.cambiar_estado, name='cambiar_estado'),
     path('borrarControl/<int:id>', views.borrarControl, name='borrarControl'),
+    path('control_estado/<str:control_nombre>/', views.obtener_estado_control, name='control_estado'),
 ]
